57a0f12d9dd8ae6fa7bd55873c85f995f85eb108
zyjqlzy <5201314Yjq>
2019-12-20 17:01:00 +0800

    登录测试修改

--- case/test_login.py
+++ case/test_login.py
@@ -2,19 +2,21 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
+import ddt
 import unittest
 from common import mysql_util
 from common import base_api
 
-op = mysql_util.OperationDbInterface()
-testdata = op.select_one_sql('select * from api where function = \'login\'')[0]['data']
-print(testdata)
-class TestLogin(unittest.TestCase):
-    def SetUp(self):
-        print(testdata)
 
-    def test_login(self,testdata):
-        r = base_api.send_requests(testdata)
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        self.op = mysql_util.OperationDbInterface()
+        self.testdata = self.op.select_one_sql('select * from api where function = \'login\'')['data']
+        print(self.testdata)
+
+    def test_login(self):
+        r = base_api.send_requests(self.testdata)
+        return r['data']
 
 
 if __name__ == "__main__":
