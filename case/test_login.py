62a2fbb70d06051eb2b1dbdc0ec1409270c337df
zyjqlzy <5201314Yjq>
2019-12-20 16:24:09 +0800

    登录测试

--- case/test_login.py
+++ case/test_login.py
@@ -3,6 +3,19 @@
 import requests
 import json
 import unittest
+from common import mysql_util
+from common import base_api
+
+op = mysql_util.OperationDbInterface()
+testdata = op.select_one_sql('select * from api where function = \'login\'')[0]['data']
+print(testdata)
+class TestLogin(unittest.TestCase):
+    def SetUp(self):
+        print(testdata)
+
+    def test_login(self,testdata):
+        r = base_api.send_requests(testdata)
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    unittest.main()
