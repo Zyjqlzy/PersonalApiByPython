4cdb130ff2f8b64a16b1d5eda6e555124a1febfd
zyjqlzy <5201314Yjq>
2019-12-20 17:01:17 +0800

    接口操作维护

--- common/base_api.py
+++ common/base_api.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import requests
+from common import mysql_util
 
 
 def send_requests(testdata):
     method = testdata['method']  # POST or GET or DELETE
     url = testdata['url']  # 接口
-    params = testdata['params']  # 参数
+    params = eval(testdata['params'])  # 参数
     test_func = testdata['function']  # 功能
     print("*******正在执行用例：-----  %s  ----**********" % test_func)
     print("请求方式：%s, 请求url:%s" % (method, url))
@@ -15,7 +16,7 @@ def send_requests(testdata):
 
     try:
         if method == "post":
-            r = requests.post(url, params)
+            r = requests.post(url, data=params)
         else:
             r = requests.get(url, params)
         result = r.json()
@@ -23,4 +24,11 @@ def send_requests(testdata):
         print(e)
 
     return result
-# if __name__ == "__main__":
+
+
+if __name__ == "__main__":
+    op = mysql_util.OperationDbInterface()
+    testdata = op.select_one_sql('select * from api where function = \'login\'')['data']
+    print(testdata)
+    req = send_requests(testdata)
+    print(req)
